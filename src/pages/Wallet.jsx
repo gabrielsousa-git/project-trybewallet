c9da64ce941fafcd7b89ae2e2aeb2074f3a04dd9
Gabriel Sousa <gabrielsousa224.gs@gmail.com>
2022-09-04 17:01:31 -0300

    Requisito 6 concluído

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
@@ -12,6 +13,10 @@ class Wallet extends React.Component {
 
         <div>
           <WalletForm />
+        </div>
+
+        <div>
+          <Table />
         </div>
       </main>
 
